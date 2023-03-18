cec835564c8ea2307278b0fe9bc4cbde5b72857e
scode234 <scodes3451@gmail.com>
2023-03-18 14:09:40 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/HIIT.java
+++ WorkoutTracker/src/com/workoutTracker/HIIT.java
@@ -1,5 +1,28 @@
 package com.workoutTracker;
 
 public class HIIT extends Exercise {
+	
+	double activeIntervalSec;
+	double restInvertalSec;
+	int numOfIntervals;
+	
+	public double getActiveIntervalSec() {
+		return activeIntervalSec;
+	}
+	public void setActiveIntervalSec(double activeIntervalSec) {
+		this.activeIntervalSec = activeIntervalSec;
+	}
+	public double getRestInvertalSec() {
+		return restInvertalSec;
+	}
+	public void setRestInvertalSec(double restInvertalSec) {
+		this.restInvertalSec = restInvertalSec;
+	}
+	public int getNumOfIntervals() {
+		return numOfIntervals;
+	}
+	public void setNumOfIntervals(int numOfIntervals) {
+		this.numOfIntervals = numOfIntervals;
+	}
 
 }
