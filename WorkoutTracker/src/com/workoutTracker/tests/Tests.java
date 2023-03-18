6ebdbe42e284520cac2e7e13dcfa93b48939ec84
scode234 <scodes3451@gmail.com>
2023-03-18 16:15:10 -0500

    Add test for convertMilesToKilometers method

--- WorkoutTracker/src/com/workoutTracker/tests/Tests.java
+++ WorkoutTracker/src/com/workoutTracker/tests/Tests.java
@@ -1,5 +1,18 @@
 package com.workoutTracker.tests;
 
+import org.junit.jupiter.api.Test;
+
+import com.workoutTracker.Run;
+
+import org.junit.Assert;
+
 public class Tests {
 
+	@Test
+	 void convert1MileToKm() {
+		Run run = new Run();
+		double converted = run.convertMilesToKilometers(1);
+		double expected = 1.61;
+		Assert.assertEquals("test", expected, converted, 0.001);
+	}
 }
