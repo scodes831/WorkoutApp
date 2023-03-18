ca54309e9aa3bb9fae6114927ab2f9efb60ba421
scode234 <scodes3451@gmail.com>
2023-03-18 13:43:34 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/User.java
+++ WorkoutTracker/src/com/workoutTracker/User.java
@@ -1,5 +1,73 @@
 package com.workoutTracker;
 
+import java.util.ArrayList;
+
 public class User {
+	
+	int userId;
+	String firstName;
+	String lastName;
+	int age;
+	double weightLbs;
+	double weightKg;
+	
+	ArrayList<Workout> workouts = new ArrayList<Workout>();
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getWeightLbs() {
+		return weightLbs;
+	}
+
+	public void setWeightLbs(double weightLbs) {
+		this.weightLbs = weightLbs;
+	}
+
+	public double getWeightKg() {
+		return weightKg;
+	}
+
+	public void setWeightKg(double weightKg) {
+		this.weightKg = weightKg;
+	}
+
+	public ArrayList<Workout> getWorkouts() {
+		return workouts;
+	}
+
+	public void setWorkouts(ArrayList<Workout> workouts) {
+		this.workouts = workouts;
+	}
+	
 
 }
