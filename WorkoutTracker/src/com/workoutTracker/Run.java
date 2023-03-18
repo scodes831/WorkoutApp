41cd9b20209fac6045f9b556d6fa7ef525bd0993
scode234 <scodes3451@gmail.com>
2023-03-18 14:01:46 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/Run.java
+++ WorkoutTracker/src/com/workoutTracker/Run.java
@@ -1,5 +1,35 @@
 package com.workoutTracker;
 
 public class Run extends Exercise implements Distance, Speed {
+	
+	double mph;
+	double pace;
+	double distanceMi;
+	double distanceKm;
+	
+	public double getMph() {
+		return mph;
+	}
+	public void setMph(double mph) {
+		this.mph = mph;
+	}
+	public double getPace() {
+		return pace;
+	}
+	public void setPace(double pace) {
+		this.pace = pace;
+	}
+	public double getDistanceMi() {
+		return distanceMi;
+	}
+	public void setDistanceMi(double distanceMi) {
+		this.distanceMi = distanceMi;
+	}
+	public double getDistanceKm() {
+		return distanceKm;
+	}
+	public void setDistanceKm(double distanceKm) {
+		this.distanceKm = distanceKm;
+	}
 
 }
