63615985dd539d145064739e4f2dd28cd2a1aa99
scode234 <scodes3451@gmail.com>
2023-03-18 13:56:58 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/StrengthTraining.java
+++ WorkoutTracker/src/com/workoutTracker/StrengthTraining.java
@@ -1,5 +1,49 @@
 package com.workoutTracker;
 
 public class StrengthTraining extends Exercise {
+	
+	String exerciseName;
+	String muscleGroup;
+	double weightLb;
+	double weightKg;
+	int sets;
+	int reps;
+	
+	public String getExerciseName() {
+		return exerciseName;
+	}
+	public void setExerciseName(String exerciseName) {
+		this.exerciseName = exerciseName;
+	}
+	public String getMuscleGroup() {
+		return muscleGroup;
+	}
+	public void setMuscleGroup(String muscleGroup) {
+		this.muscleGroup = muscleGroup;
+	}
+	public double getWeightLb() {
+		return weightLb;
+	}
+	public void setWeightLb(double weightLb) {
+		this.weightLb = weightLb;
+	}
+	public double getWeightKg() {
+		return weightKg;
+	}
+	public void setWeightKg(double weightKg) {
+		this.weightKg = weightKg;
+	}
+	public int getSets() {
+		return sets;
+	}
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+	public int getReps() {
+		return reps;
+	}
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
 
 }
