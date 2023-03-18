0d0c4b93d339dcd7b8bdc3eaa6ef8217c1cd0cb9
scode234 <scodes3451@gmail.com>
2023-03-18 14:06:18 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/Bike.java
+++ WorkoutTracker/src/com/workoutTracker/Bike.java
@@ -1,5 +1,49 @@
 package com.workoutTracker;
 
 public class Bike extends Exercise implements Distance, Speed {
+	
+	double mph;
+	double pace;
+	double distanceMi;
+	double distanceKg;
+	int resistance;
+	boolean isStationaryBike;
+	
+	public double getMph() {
+		return mph;
+	}
+	public void setMph(double mph) {
+		this.mph = mph;
+	}
+	public double getPace() {
+		return pace;
+	}
+	public void setPace(double pace) {
+		this.pace = pace;
+	}
+	public double getDistanceMi() {
+		return distanceMi;
+	}
+	public void setDistanceMi(double distanceMi) {
+		this.distanceMi = distanceMi;
+	}
+	public double getDistanceKg() {
+		return distanceKg;
+	}
+	public void setDistanceKg(double distanceKg) {
+		this.distanceKg = distanceKg;
+	}
+	public int getResistance() {
+		return resistance;
+	}
+	public void setResistance(int resistance) {
+		this.resistance = resistance;
+	}
+	public boolean isStationaryBike() {
+		return isStationaryBike;
+	}
+	public void setStationaryBike(boolean isStationaryBike) {
+		this.isStationaryBike = isStationaryBike;
+	}
 
 }
