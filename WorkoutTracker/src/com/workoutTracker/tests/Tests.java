b6851750ea10275b8945076e394b04136feeec7a
scode234 <scodes3451@gmail.com>
2023-03-18 16:22:25 -0500

    Add tests for convertPoundsToKilograms

--- WorkoutTracker/src/com/workoutTracker/tests/Tests.java
+++ WorkoutTracker/src/com/workoutTracker/tests/Tests.java
@@ -1,10 +1,10 @@
 package com.workoutTracker.tests;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.workoutTracker.Run;
-
-import org.junit.Assert;
+import com.workoutTracker.StrengthTraining;
 
 public class Tests {
 
@@ -22,5 +22,21 @@ public class Tests {
 		double converted = run.convertMilesToKilometers(5);
 		double expected = 8.05;
 		Assert.assertEquals("Incorrect conversion to km", expected, converted, 0.001);
+	}
+	
+	@Test
+	void convert1PoundToKg() {
+		StrengthTraining st = new StrengthTraining();
+		double converted = st.convertPoundsToKilograms(1);
+		double expected = 0.45;
+		Assert.assertEquals("Incorrect conversion to kg", expected, converted, 0.001);
+	}
+	
+	@Test
+	void convert5PoundToKg() {
+		StrengthTraining st = new StrengthTraining();
+		double converted = st.convertPoundsToKilograms(5);
+		double expected = 2.27;
+		Assert.assertEquals("Incorrect conversion to kg", expected, converted, 0.001);
 	}
 }
