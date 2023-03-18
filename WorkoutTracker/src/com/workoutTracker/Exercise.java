d36bd2f5f8146a438be6d517f8ffcbc1af7aaeaa
scode234 <scodes3451@gmail.com>
2023-03-18 14:04:40 -0500

    Add variables, getters, and setters

--- WorkoutTracker/src/com/workoutTracker/Exercise.java
+++ WorkoutTracker/src/com/workoutTracker/Exercise.java
@@ -1,5 +1,21 @@
 package com.workoutTracker;
 
 public abstract class Exercise {
+	
+	int heartRate;
+	int calories;
+	
+	public int getHeartRate() {
+		return heartRate;
+	}
+	public void setHeartRate(int heartRate) {
+		this.heartRate = heartRate;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
 
 }
