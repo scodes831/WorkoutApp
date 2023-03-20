97aba59299176da89880cdbaf67ca7430637642b
scode234 <scodes3451@gmail.com>
2023-03-19 19:48:05 -0500

    Add variable, getter, and setter

--- WorkoutTracker/src/com/workoutTracker/UserManager.java
+++ WorkoutTracker/src/com/workoutTracker/UserManager.java
@@ -1,5 +1,17 @@
 package com.workoutTracker;
 
+import java.util.ArrayList;
+
 public class UserManager {
+	
+	ArrayList<User> users = new ArrayList<User>();
+
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
 
 }
