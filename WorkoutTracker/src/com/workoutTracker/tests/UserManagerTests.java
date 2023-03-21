d27b01e69f626270b6dc2bbbd513606f2994189d
scode234 <scodes3451@gmail.com>
2023-03-20 20:09:18 -0500

    Add test for addNewUser

--- WorkoutTracker/src/com/workoutTracker/tests/UserManagerTests.java
+++ WorkoutTracker/src/com/workoutTracker/tests/UserManagerTests.java
@@ -1,5 +1,17 @@
 package com.workoutTracker.tests;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import com.workoutTracker.UserManager;
+
 public class UserManagerTests {
+	
+	@Test
+	void newUserGetsAdded() {
+		UserManager userManager = new UserManager();
+		userManager.addNewUser("Jessica", "Green", 29, 138);
+		Assert.assertTrue("user is not added to list", userManager.getUsers().size() == 1);
+	}
 
 }
