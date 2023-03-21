75161b708e6a7c78d946d77a8efa25d7da11b2b8
scode234 <scodes3451@gmail.com>
2023-03-20 20:08:52 -0500

    Add addNewUser method

--- WorkoutTracker/src/com/workoutTracker/UserManager.java
+++ WorkoutTracker/src/com/workoutTracker/UserManager.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class UserManager {
 	
 	ArrayList<User> users = new ArrayList<User>();
+	
+	public void addNewUser(String firstName, String lastName, int age, double weight) {
+		User user = new User(firstName, lastName, age, weight);
+		getUsers().add(user);
+	}
 
 	public ArrayList<User> getUsers() {
 		return users;
