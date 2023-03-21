f8a42c47f1738ccdd14cc79942f58523619cb0f3
scode234 <scodes3451@gmail.com>
2023-03-20 20:00:06 -0500

    Separate tests for Exercise class and refactor tests with BeforeEach

--- WorkoutTracker/src/com/workoutTracker/tests/ExerciseTests.java
+++ WorkoutTracker/src/com/workoutTracker/tests/ExerciseTests.java
@@ -1,5 +1,50 @@
 package com.workoutTracker.tests;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.workoutTracker.Run;
+import com.workoutTracker.StrengthTraining;
+
+
 public class ExerciseTests {
+	
+	Run run;
+	StrengthTraining st;
+	
+	@BeforeEach
+	public void setup() {
+		run = new Run();
+		st = new StrengthTraining();
+	}
+	
+	@Test
+	void convert1MileToKm() {
+		double converted = run.convertMilesToKilometers(1);
+		double expected = 1.61;
+		Assert.assertEquals("Incorrect conversion to km", expected, converted, 0.001);
+	}
+
+	@Test
+	void convert5MilesToKm() {
+		double converted = run.convertMilesToKilometers(5);
+		double expected = 8.05;
+		Assert.assertEquals("Incorrect conversion to km", expected, converted, 0.001);
+	}
+
+	@Test
+	void convert1PoundToKg() {
+		double converted = st.convertPoundsToKilograms(1);
+		double expected = 0.45;
+		Assert.assertEquals("Incorrect conversion to kg", expected, converted, 0.001);
+	}
+
+	@Test
+	void convert5PoundToKg() {
+		double converted = st.convertPoundsToKilograms(5);
+		double expected = 2.27;
+		Assert.assertEquals("Incorrect conversion to kg", expected, converted, 0.001);
+	}
 
 }
