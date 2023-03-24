a4294e93514e85c08ec3dc8793aa378c79484f33
scode234 <scodes3451@gmail.com>
2023-03-23 19:19:56 -0500

    Add methods for age and weight validation and call them during user prompt methods

--- WorkoutTracker/src/com/workoutTracker/UserPrompts.java
+++ WorkoutTracker/src/com/workoutTracker/UserPrompts.java
@@ -4,29 +4,58 @@ import java.util.Scanner;
 
 public class UserPrompts {
 	
-	
-	public static String askFirstName(Scanner in) {
+	private static String askFirstName(Scanner in) {
 		System.out.println("Enter first name:");
 		String firstName = in.next();
 		return firstName;
 	}
 	
-	public static String askLastName(Scanner in) {
+	private static String askLastName(Scanner in) {
 		System.out.println("Enter last name:");
 		String lastName = in.next();
 		return lastName;
 	}
 	
-	public static int askAge(Scanner in) {
+	private static int askAge(Scanner in) {
 		System.out.println("Enter age:");
-		int age = in.nextInt();
-		return age;
+		boolean inputError = false;
+		do {
+			int age = in.nextInt();
+			if (validateAgeInput(age)) {
+				return age;
+			} else {
+				inputError = true;
+				System.out.println("Please enter a valid age between 1 and 100");
+			}
+			
+		} while (inputError);
+		return 0;
 	}
 	
-	public static double askWeight(Scanner in) {
+	private static double askWeight(Scanner in) {
 		System.out.println("Enter weight in pounds:");
-		double weight = in.nextDouble();
-		return weight;
+		boolean inputError = false;
+		do {
+			double weight = in.nextDouble();
+			if (validateWeightInput(weight)) {
+				return weight;
+			} else {
+				inputError = true;
+				System.out.println("Please enter a valid weight greater than 0");
+			}
+
+		} while (inputError);
+		return 0;
+	}
+	
+	private static boolean validateAgeInput(int age) {
+		boolean isValidAge = (age < 100 && age > 1) ? true : false;
+		return isValidAge;
+	}
+	
+	private static boolean validateWeightInput(double weight) {
+		boolean isValidWeight = (weight > 0) ? true : false;
+		return isValidWeight;
 	}
 	
 
