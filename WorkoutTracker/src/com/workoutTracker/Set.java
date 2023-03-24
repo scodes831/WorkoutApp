8f67f66c37a8b73a464a09c68634e1771215de4a
scode234 <scodes3451@gmail.com>
2023-03-23 19:49:07 -0500

    Add variables, getters, setters, and constructor

--- WorkoutTracker/src/com/workoutTracker/Set.java
+++ WorkoutTracker/src/com/workoutTracker/Set.java
@@ -1,5 +1,41 @@
 package com.workoutTracker;
 
 public class Set {
+	
+	int sets;
+	double weightLbs;
+	double weightKg;
+	int reps;
+	
+	public Set(int sets, double weightLbs, int reps) {
+		this.sets = sets;
+		this.weightLbs = weightLbs;
+		this.reps = reps;
+	}
+	
+	public int getSets() {
+		return sets;
+	}
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+	public double getWeightLbs() {
+		return weightLbs;
+	}
+	public void setWeightLbs(double weightLbs) {
+		this.weightLbs = weightLbs;
+	}
+	public double getWeightKg() {
+		return weightKg;
+	}
+	public void setWeightKg(double weightKg) {
+		this.weightKg = weightKg;
+	}
+	public int getReps() {
+		return reps;
+	}
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
 
 }
