52ef61d6e1309c00eeae77288a94a2328a262cfd
scode234 <scodes3451@gmail.com>
2023-03-24 20:40:21 -0500

    Add methods for user to select a menu

--- WorkoutTracker/src/com/workoutTracker/MainMenu.java
+++ WorkoutTracker/src/com/workoutTracker/MainMenu.java
@@ -1,5 +1,39 @@
 package com.workoutTracker;
 
-public class MainMenu {
+import java.util.Scanner;
+
+public class MainMenu extends Menu {
+	
+	public void displayMenu(UserManager userManager, MainMenu mainMenu) {
+		boolean inputError = false;
+		do {
+			try {
+				int selection = makeSelection();
+				processSelection(userManager, mainMenu, selection);
+			} catch (Exception e) {
+				inputError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (inputError);
+		
+	}
+	
+	public int makeSelection() {
+		System.out.println("Menu options:\n1 - Users\n2 - Workouts");
+		Scanner in = new Scanner(System.in);
+		int selection = in.nextInt();
+		return selection;
+	}
+
+	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection) {
+		switch (selection) {
+		case 1: 
+			UserMenu userMenu = new UserMenu();
+			userMenu.displayMenu(userManager, mainMenu);
+		}
+		
+	}
+	
+	
 
 }
