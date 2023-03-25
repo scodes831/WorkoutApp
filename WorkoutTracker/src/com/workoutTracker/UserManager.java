1c4513ffcdfc99986369c8b1936efdf4abc8cfca
scode234 <scodes3451@gmail.com>
2023-03-24 21:10:39 -0500

    Add getNewUserDetails method

--- WorkoutTracker/src/com/workoutTracker/UserManager.java
+++ WorkoutTracker/src/com/workoutTracker/UserManager.java
@@ -1,14 +1,26 @@
 package com.workoutTracker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserManager {
 	
 	ArrayList<User> users = new ArrayList<User>();
 	
-	public void addNewUser(String firstName, String lastName, int age, double weight) {
-		User user = new User(firstName, lastName, age, weight);
+	public ArrayList<Object> getNewUserDetails() {
+		Scanner in = new Scanner(System.in);
+		ArrayList<Object> values = new ArrayList<Object>();
+		values.add(UserPrompts.askFirstName(in));
+		values.add(UserPrompts.askLastName(in));
+		values.add(UserPrompts.askAge(in));
+		values.add(UserPrompts.askWeight(in, true));
+		return values;
+	}
+	
+	public User addNewUser(ArrayList<Object> values) {
+		User user = new User(values.get(0), values.get(1), values.get(2), values.get(3));
 		getUsers().add(user);
+		return user;
 	}
 
 	public ArrayList<User> getUsers() {
