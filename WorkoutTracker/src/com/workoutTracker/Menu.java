a98079a24afc848db4bdc5f9f9154d6c0d13d5fe
scode234 <scodes3451@gmail.com>
2023-03-24 20:39:58 -0500

    Make abstract and add abstract methods

--- WorkoutTracker/src/com/workoutTracker/Menu.java
+++ WorkoutTracker/src/com/workoutTracker/Menu.java
@@ -1,5 +1,9 @@
 package com.workoutTracker;
 
 public abstract class Menu {
+	
+	public abstract void displayMenu(UserManager userManager, MainMenu mainMenu);
+	
+	public abstract void processSelection(UserManager userManager, MainMenu mainMenu, int selection);
 
 }
