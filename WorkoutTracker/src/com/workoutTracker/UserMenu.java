9a2f75c5a2c8b674eba37d39df49b38dd9a71c59
scode234 <scodes3451@gmail.com>
2023-03-24 21:10:01 -0500

    Add logic for user selections and adding a new user

--- WorkoutTracker/src/com/workoutTracker/UserMenu.java
+++ WorkoutTracker/src/com/workoutTracker/UserMenu.java
@@ -1,5 +1,45 @@
 package com.workoutTracker;
 
-public class UserMenu {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserMenu extends Menu {
+
+	public void displayMenu(UserManager userManager, MainMenu mainMenu) {
+		boolean inputError = false;
+		do {
+			try {
+				int selection = makeSelection();
+				processSelection(userManager, mainMenu, selection);
+			} catch (Exception e) {
+				inputError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (inputError);
+
+	}
+
+	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection) {
+		switch (selection) {
+		case 1:
+			ArrayList<Object> values = userManager.getNewUserDetails();
+			User user = userManager.addNewUser(values);
+			System.out.println("You have added a new user: " + user.getFirstName() + " " + user.getLastName() + " is "
+					+ user.getAge() + " years old and weighs " + user.getWeightLbs() + "pounds.");
+		case 2:
+			// display users
+		case 3:
+			// display, then edit users;
+
+		}
+
+	}
+
+	public int makeSelection() {
+		System.out.println("User Menu Options:\n1 - Add User\n2 - Display Users\n3 - Edit Users");
+		Scanner in = new Scanner(System.in);
+		int selection = in.nextInt();
+		return selection;
+	}
 
 }
