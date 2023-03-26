69f07703e7f46011ee1188e6628a84acd674795b
scode234 <scodes3451@gmail.com>
2023-03-25 19:50:06 -0500

    Update name to displayUsers and add parameter for ArrayList of User objects

--- WorkoutTracker/src/com/workoutTracker/UserManager.java
+++ WorkoutTracker/src/com/workoutTracker/UserManager.java
@@ -1,5 +1,7 @@
 package com.workoutTracker;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.Scanner;
@@ -23,6 +25,15 @@ public class UserManager {
 		getUsers().add(user);
 		return user;
 	}
+	
+	public void displayUsers(ArrayList<User> users) {
+		Formatter userTable = new Formatter();
+		userTable.format("%15s %15s %15s %15s %15s %15s\n", "User Id", "First Name", "Last Name", "Age", "Weight (lbs)", "Weight (kg)");
+		for (User user : users) {
+			userTable.format("%15s %15s %15s %15s %15s %15s\n", user.getUserId(), user.getFirstName(), user.getLastName(), user.getAge(), user.getWeightLbs(), user.getWeightKg());
+		}
+		System.out.println(userTable);
+	}
 
 	public ArrayList<User> getUsers() {
 		return users;
@@ -30,16 +41,6 @@ public class UserManager {
 
 	public void setUsers(ArrayList<User> users) {
 		this.users = users;
-	}
-
-	public void displayAllUsers() {
-		Formatter userTable = new Formatter();
-		userTable.format("%15s %15s %15s %15s %15s %15s\n", "User Id", "First Name", "Last Name", "Age", "Weight (lbs)", "Weight (kg)");
-		for (User user : getUsers()) {
-			userTable.format("%15s %15s %15s %15s %15s %15s\n", user.getUserId(), user.getFirstName(), user.getLastName(), user.getAge(), user.getWeightLbs(), user.getWeightKg());
-		}
-		System.out.println(userTable);
-		
 	}
 
 }
