c9e1ed9e6e3fc33b5454ca0b441d110247d9059a
scode234 <scodes3451@gmail.com>
2023-03-25 19:37:17 -0500

    Add displayAllUsers method

--- WorkoutTracker/src/com/workoutTracker/UserManager.java
+++ WorkoutTracker/src/com/workoutTracker/UserManager.java
@@ -1,6 +1,7 @@
 package com.workoutTracker;
 
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class UserManager {
@@ -29,6 +30,16 @@ public class UserManager {
 
 	public void setUsers(ArrayList<User> users) {
 		this.users = users;
+	}
+
+	public void displayAllUsers() {
+		Formatter userTable = new Formatter();
+		userTable.format("%15s %15s %15s %15s %15s %15s\n", "User Id", "First Name", "Last Name", "Age", "Weight (lbs)", "Weight (kg)");
+		for (User user : getUsers()) {
+			userTable.format("%15s %15s %15s %15s %15s %15s\n", user.getUserId(), user.getFirstName(), user.getLastName(), user.getAge(), user.getWeightLbs(), user.getWeightKg());
+		}
+		System.out.println(userTable);
+		
 	}
 
 }
