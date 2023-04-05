affca88afa948f901ded8f0d021b15af245fc3a7
scode234 <scodes3451@gmail.com>
2023-04-04 19:53:16 -0500

    Initial commit

---
+++ WorkoutTracker/src/com/workoutTracker/WorkoutMenu.java
@@ -0,0 +1,23 @@
+package com.workoutTracker;
+
+public class WorkoutMenu extends Menu {
+
+	@Override
+	public void displayMenu(UserManager userManager, MainMenu mainMenu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int makeSelection() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
