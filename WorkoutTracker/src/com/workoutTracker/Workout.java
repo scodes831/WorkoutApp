fc10e19839445210f6717c52676f4d49565315a6
scode234 <scodes3451@gmail.com>
2023-04-05 19:47:49 -0500

    Add second constructor

--- WorkoutTracker/src/com/workoutTracker/Workout.java
+++ WorkoutTracker/src/com/workoutTracker/Workout.java
@@ -4,49 +4,62 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Workout {
-	
+
 	private static int ID = 49820;
-	
+
 	int workoutId;
 	LocalDate date;
 	int time;
 	ArrayList<Exercise> exercises = new ArrayList<Exercise>();
-	
-	public Workout (LocalDate date, int time, ArrayList<Exercise> exercises) {
+
+	public Workout(LocalDate date, int time, ArrayList<Exercise> exercises) {
 		this.workoutId = generateWorkoutId();
 		this.date = date;
 		this.time = time;
 		this.exercises = exercises;
 	}
-	
+
+	public Workout(LocalDate date, int time) {
+		this.workoutId = generateWorkoutId();
+		this.date = date;
+		this.time = time;
+	}
+
 	public int generateWorkoutId() {
 		int id = Workout.getID();
 		id++;
 		Workout.setID(id);
 		return id;
 	}
-	
+
 	public int getWorkoutId() {
 		return workoutId;
 	}
+
 	public void setWorkoutId(int workoutId) {
 		this.workoutId = workoutId;
 	}
+
 	public LocalDate getDate() {
 		return date;
 	}
+
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
 	public int getTime() {
 		return time;
 	}
+
 	public void setTime(int time) {
 		this.time = time;
 	}
+
 	public ArrayList<Exercise> getExercises() {
 		return exercises;
 	}
+
 	public void setExercises(ArrayList<Exercise> exercises) {
 		this.exercises = exercises;
 	}
