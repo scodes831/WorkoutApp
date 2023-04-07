5804b3d763994dd3cc6b90251d7ea43162ed34df
scode234 <scodes3451@gmail.com>
2023-04-06 19:28:16 -0500

    Add constructor, exericseId variable, getter and setter

--- WorkoutTracker/src/com/workoutTracker/Exercise.java
+++ WorkoutTracker/src/com/workoutTracker/Exercise.java
@@ -7,8 +7,20 @@ public abstract class Exercise {
 	
 	private static int ID = 378832;
 	
+	int exerciseId;
 	int heartRate;
 	int calories;
+	
+	Exercise() {
+		this.exerciseId = generateExerciseId();
+	}
+	
+	public int generateExerciseId() {
+		int id = Exercise.getID();
+		id++;
+		Exercise.setID(id);
+		return id;
+	}
 	
 	public double convertMilesToKilometers(double miles) {
 		BigDecimal km = new BigDecimal(miles * 1.609344).setScale(2, RoundingMode.HALF_UP);
@@ -31,6 +43,14 @@ public abstract class Exercise {
 	}
 	public void setCalories(int calories) {
 		this.calories = calories;
+	}
+	
+	public int getExerciseId() {
+		return exerciseId;
+	}
+	
+	public void setExerciseId(int exerciseId) {
+		this.exerciseId = exerciseId;
 	}
 
 	public static int getID() {
