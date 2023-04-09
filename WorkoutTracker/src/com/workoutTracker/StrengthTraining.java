bb2be4444bf6f72a9033ccae99d636da5beba884
scode234 <scodes3451@gmail.com>
2023-04-08 19:50:42 -0500

    Add addExerciseDetails and addSetDetails methods

--- WorkoutTracker/src/com/workoutTracker/StrengthTraining.java
+++ WorkoutTracker/src/com/workoutTracker/StrengthTraining.java
@@ -16,9 +16,22 @@ public class StrengthTraining extends Exercise {
 	
 	StrengthTraining() {}
 	
-	public void addNewSet(int sets, double weightLbs, int reps) {
-		Set set = new Set(sets, weightLbs, reps);
-		getSets().add(set);
+	public void addExerciseDetails(Workout workout) {
+		String exerciseName = UserPrompts.askStrengthTrainingExerciseName();
+		String muscleGroup = UserPrompts.askMuscleGroupName();
+		this.setExerciseName(exerciseName);
+		this.setMuscleGroup(muscleGroup);
+		addSetDetails();
+	}
+	
+	public void addSetDetails() {
+		double setWeightLbs = UserPrompts.askSetWeight();
+		int sets = UserPrompts.askNumSets();
+		int reps = UserPrompts.askSetReps();
+		for (int i = 1; i < sets+1; i++) {
+			Set set = new Set(setWeightLbs, reps);
+			getSets().add(set);
+		}
 	}
 		
 	public String getExerciseName() {
