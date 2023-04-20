583ac1d1ffcbb8cf42ae9833177521b352217a92
scode234 <scodes3451@gmail.com>
2023-04-19 20:25:52 -0500

    Add logic for edit workout menu option, call editWorkout method

--- WorkoutTracker/src/com/workoutTracker/WorkoutMenu.java
+++ WorkoutTracker/src/com/workoutTracker/WorkoutMenu.java
@@ -1,23 +1,50 @@
 package com.workoutTracker;
 
+import java.util.Scanner;
+
 public class WorkoutMenu extends Menu {
 
-	@Override
 	public void displayMenu(UserManager userManager, MainMenu mainMenu) {
-		// TODO Auto-generated method stub
-		
+		boolean inputError = false;
+		do {
+			try {
+				int selection = makeSelection();
+				processSelection(userManager, mainMenu, selection);
+			} catch (Exception e) {
+				inputError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (inputError);
+
 	}
 
-	@Override
-	public int makeSelection() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection) {
-		// TODO Auto-generated method stub
-		
+		User selectedUser = userManager.selectAUser(userManager);
+		switch (selection) {
+		case 1:
+			Workout workout = selectedUser.addWorkout(selectedUser);
+			workout.addWorkoutDetails(workout);
+			break;
+		case 2:
+			selectedUser.displayAllWorkouts();
+			break;
+		case 3:
+			selectedUser.displayAllWorkouts();
+			Workout selectedWorkout = selectedUser.selectWorkout();
+			selectedWorkout.editWorkout();
+			break;
+		case 4:
+			mainMenu.displayMenu(userManager, mainMenu);
+		}
+		displayMenu(userManager, mainMenu);
+	}
+
+	public int makeSelection() {
+		System.out.println(
+				"Workout Menu Options:\n1 - Add Workout\n2 - Display Workouts\n3 - Edit Workouts\n4 - Back to Main Menu");
+		Scanner in = new Scanner(System.in);
+		int selection = in.nextInt();
+		return selection;
 	}
 
 }
