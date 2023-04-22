fbb82d3a9488b7794509e3a500e2e6356d3f4294
scode234 <scodes3451@gmail.com>
2023-04-22 17:49:44 -0500

    Call connectDatabase to connect to workouttracker db

--- WorkoutTracker/src/com/workoutTracker/Main.java
+++ WorkoutTracker/src/com/workoutTracker/Main.java
@@ -1,10 +1,10 @@
 package com.workoutTracker;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
+		DatabaseManager dm = new DatabaseManager();
+		dm.connectDatabase("workouttracker", null, null);
 		UserManager um = new UserManager();
 		MainMenu mainMenu = new MainMenu();
 		UserMenu userMenu = new UserMenu();
