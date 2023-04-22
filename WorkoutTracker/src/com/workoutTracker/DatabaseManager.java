bfab71a0f84250fe4017f7fd5558925c2c658d45
scode234 <scodes3451@gmail.com>
2023-04-22 17:49:17 -0500

    Add connectDatabase method

--- WorkoutTracker/src/com/workoutTracker/DatabaseManager.java
+++ WorkoutTracker/src/com/workoutTracker/DatabaseManager.java
@@ -1,5 +1,19 @@
 package com.workoutTracker;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DatabaseManager {
+	
+	public Connection connectDatabase(String database, String user, String pw) {
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + database, user, pw);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return connection;
+	}
 
 }
