4505612d4ab528d0d480b0fa60101e6e5462cf83
scode234 <scodes3451@gmail.com>
2023-04-21 20:25:36 -0500

    Add editHIITDetails method

--- WorkoutTracker/src/com/workoutTracker/HIIT.java
+++ WorkoutTracker/src/com/workoutTracker/HIIT.java
@@ -15,6 +15,43 @@ public class HIIT extends Exercise {
 		this.setNumOfIntervals(numOfIntervals);
 	}
 	
+	public void editHIITDetails() {
+		boolean stillEditing = true;
+		do {
+			int selection = UserPrompts.askHIITEditFields();
+			if (selection > 0) {
+				switch (selection) {
+				case 1:
+					int newExerciseTime = UserPrompts.askTime("exercise");
+					this.setExerciseTime(newExerciseTime);
+					break;
+				case 2:
+					int newHeartRate = UserPrompts.askHeartRate();
+					this.setHeartRate(newHeartRate);
+					break;
+				case 3:
+					int calories = UserPrompts.askCalories();
+					this.setCalories(calories);
+					break;
+				case 4:
+					int newActiveIntervalSec = UserPrompts.askIntervalSeconds("active");
+					this.setActiveIntervalSec(newActiveIntervalSec);
+					break;
+				case 5:
+					int newRestIntervalSec = UserPrompts.askIntervalSeconds("rest");
+					this.setRestIntervalSec(newRestIntervalSec);
+					break;
+				case 6:
+					int newNumOfIntervals = UserPrompts.askNumIntervals();
+					this.setNumOfIntervals(newNumOfIntervals);
+					break;
+				}
+			} else {
+				stillEditing = false;
+			}
+		} while (stillEditing);
+	}
+	
 	public int getActiveIntervalSec() {
 		return activeIntervalSec;
 	}
@@ -33,5 +70,4 @@ public class HIIT extends Exercise {
 	public void setNumOfIntervals(int numOfIntervals) {
 		this.numOfIntervals = numOfIntervals;
 	}
-
 }
