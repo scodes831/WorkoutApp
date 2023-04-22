d3814de17eedbd191df765e535c11f0ce6fd6758
scode234 <scodes3451@gmail.com>
2023-04-21 20:53:20 -0500

    Call methods

--- WorkoutTracker/src/com/workoutTracker/Main.java
+++ WorkoutTracker/src/com/workoutTracker/Main.java
@@ -1,9 +1,13 @@
 package com.workoutTracker;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		UserManager um = new UserManager();
+		MainMenu mainMenu = new MainMenu();
+		UserMenu userMenu = new UserMenu();
+		mainMenu.displayMenu(um, mainMenu);
 	}
-
 }
