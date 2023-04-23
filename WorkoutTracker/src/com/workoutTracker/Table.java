72970f007f91d79baffbbf90c3a742cd0b830782
scode234 <scodes3451@gmail.com>
2023-04-23 18:18:38 -0500

    Make abstract class

--- WorkoutTracker/src/com/workoutTracker/Table.java
+++ WorkoutTracker/src/com/workoutTracker/Table.java
@@ -1,19 +1,7 @@
 package com.workoutTracker;
 
-import java.sql.Connection;
-import java.sql.Statement;
-
-public class Table {
+public abstract class Table {
 	
-	public void createTable(Connection connection, String tableName, String columns) {
-		Statement statement;
-		try {
-			String query = "create table if not exists " + tableName + " " + columns;
-			statement = connection.createStatement();
-			statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-
+	public void createTable() {};
+	
 }
