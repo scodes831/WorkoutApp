0abc742e1caecb08684d10bab23a829cc639c40f
scode234 <scodes3451@gmail.com>
2023-04-23 17:40:33 -0500

    Add createTable method

--- WorkoutTracker/src/com/workoutTracker/Table.java
+++ WorkoutTracker/src/com/workoutTracker/Table.java
@@ -1,5 +1,19 @@
 package com.workoutTracker;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class Table {
+	
+	public void createTable(Connection connection, String tableName, String columns) {
+		Statement statement;
+		try {
+			String query = "create table if not exists " + tableName + " " + columns;
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
