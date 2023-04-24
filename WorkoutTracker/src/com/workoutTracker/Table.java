39cfb758c92c1d6ca08385b3aef18281867502da
scode234 <scodes3451@gmail.com>
2023-04-23 20:40:46 -0500

    Add abstract insertRow method

--- WorkoutTracker/src/com/workoutTracker/Table.java
+++ WorkoutTracker/src/com/workoutTracker/Table.java
@@ -1,7 +1,12 @@
 package com.workoutTracker;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public abstract class Table {
 	
-	public abstract void createTable();
+	public abstract void createTable(Connection connection);
+	
+	public abstract void insertRow(Connection connection, ArrayList<Object> values);
 	
 }
