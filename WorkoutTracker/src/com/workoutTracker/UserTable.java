8e545dc7e7dfd8912a31ab7c73d8d0b37248e83c
scode234 <scodes3451@gmail.com>
2023-04-23 20:40:17 -0500

    Change numeric type precision to 6, add insertRow method

--- WorkoutTracker/src/com/workoutTracker/UserTable.java
+++ WorkoutTracker/src/com/workoutTracker/UserTable.java
@@ -2,6 +2,7 @@ package com.workoutTracker;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class UserTable extends Table {
 
@@ -9,7 +10,20 @@ public class UserTable extends Table {
 		Statement statement;
 		try {
 			String query = "create table if not exists users (userId SERIAL PRIMARY KEY, firstName VARCHAR(30), lastName VARCHAR (30), age INTEGER, "
-					+ "weightLbs NUMERIC(3,1), weightKgs NUMERIC(3,1))";
+					+ "weightLbs NUMERIC(6,1), weightKgs NUMERIC(6,1))";
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void insertRow(Connection connection, ArrayList<Object> values) {
+		Statement statement;
+		try {
+			String query = String.format(
+					"insert into users (firstName, lastName, age, weightLbs, weightKgs) values ('%s', '%s', '%s', '%s', '%s');",
+					values.get(0), values.get(1), values.get(2), values.get(3), values.get(4));
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
