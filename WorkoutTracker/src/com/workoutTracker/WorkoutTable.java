7f5c9e85fd500a703939d28ac98fc58ef1a233b2
scode234 <scodes3451@gmail.com>
2023-04-25 20:20:18 -0500

    Update insertRow to use proper variables for workout

--- WorkoutTracker/src/com/workoutTracker/WorkoutTable.java
+++ WorkoutTracker/src/com/workoutTracker/WorkoutTable.java
@@ -2,6 +2,7 @@ package com.workoutTracker;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class WorkoutTable extends Table {
 	
@@ -10,6 +11,19 @@ public class WorkoutTable extends Table {
 		try {
 			String query = "create table if not exists workouts (workoutId SERIAL PRIMARY KEY, userId SERIAL, date DATE, time INTEGER, "
 					+ "FOREIGN KEY (userId) REFERENCES users(userId))";
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void insertRow(Connection connection, ArrayList<Object> values) {
+		Statement statement;
+		try {
+			String query = String.format(
+					"insert into workouts (date, time) values ('%s', '%s');",
+					values.get(0), values.get(1));
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
