2e9f40b0caae247b464697ceb171d1e368b46cf6
scode234 <scodes3451@gmail.com>
2023-04-28 19:07:42 -0500

    Initial commit

---
+++ WorkoutTracker/src/com/workoutTracker/StrengthTrainingTable.java
@@ -0,0 +1,16 @@
+package com.workoutTracker;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public class StrengthTrainingTable extends Table {
+
+	public void createTable(Connection connection) {
+		
+	}
+
+	public void insertRow(Connection connection, ArrayList<Object> values) {
+		
+	}
+
+}
