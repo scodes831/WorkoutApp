b2f0699454f06340bd200e50ac484c2607d28488
scode234 <scodes3451@gmail.com>
2023-05-01 19:09:49 -0500

    Add readTable method

--- WorkoutTracker/src/com/workoutTracker/UserTable.java
+++ WorkoutTracker/src/com/workoutTracker/UserTable.java
@@ -1,6 +1,7 @@
 package com.workoutTracker;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -26,6 +27,39 @@ public class UserTable extends Table {
 					values.get(0), values.get(1), values.get(2), values.get(3), values.get(4));
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void readTable(Connection connection, UserManager userManager, WorkoutTable workoutTable, ExerciseTable exerciseTable, StrengthTrainingTable stTable) {
+		Statement statement;
+		ResultSet result;
+		try {
+			String query = "select * from users";
+			statement = connection.createStatement();
+			result = statement.executeQuery(query);
+			while (result.next()) {
+				boolean alreadyExists = false;
+				int userId = Integer.valueOf(result.getString("userId"));
+				String firstName = result.getString("firstName");
+				String lastName = result.getString("lastName");
+				int age = Integer.valueOf(result.getString("age"));
+				double weightLbs = Double.valueOf(result.getString("weightLbs"));
+				double weightKgs = Double.valueOf(result.getString("weightKgs"));
+				for (User user : userManager.getUsers()) {
+					if (user.getUserId() == userId) {
+						alreadyExists = true;
+					}
+				}
+				if (!alreadyExists) {
+					User user = new User(firstName, lastName, age, weightLbs);
+					user.setUserId(userId);
+					user.setWeightKg(weightKgs);
+					userManager.getUsers().add(user);
+					workoutTable.readTable(connection, user, exerciseTable, stTable);
+				}
+			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
