42daa9fa389a7de6de11223f471eebe62d0cab67
scode234 <scodes3451@gmail.com>
2023-05-01 19:11:06 -0500

    Add readTable method

--- WorkoutTracker/src/com/workoutTracker/WorkoutTable.java
+++ WorkoutTracker/src/com/workoutTracker/WorkoutTable.java
@@ -1,11 +1,13 @@
 package com.workoutTracker;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class WorkoutTable extends Table {
-	
+
 	public void createTable(Connection connection) {
 		Statement statement;
 		try {
@@ -30,5 +32,46 @@ public class WorkoutTable extends Table {
 			System.out.println(e);
 		}
 	}
-	
+
+	public void readTable(Connection connection, User user, ExerciseTable exerciseTable, StrengthTrainingTable stTable) {
+		Statement statement;
+		ResultSet result = null;
+		System.out.println("the user's id is " + user.getUserId());
+		try {
+			String query = "select * from workouts where userId = " + user.getUserId();
+			statement = connection.createStatement();
+			result = statement.executeQuery(query);
+			while (result.next()) {
+				boolean alreadyExists = false;
+				int workoutId = Integer.valueOf(result.getString("workoutId"));
+				String[] dateStr = result.getString("date").split("-");
+				int year = Integer.valueOf(dateStr[0]);
+				int month = Integer.valueOf(dateStr[1]);
+				int day = Integer.valueOf(dateStr[2]);
+				LocalDate date = LocalDate.of(year, month, day);
+				int time = Integer.valueOf(result.getString("time"));
+				int heartRate = Integer.valueOf(result.getString("heartrate"));
+				int calories = Integer.valueOf(result.getString("calories"));
+				System.out.println("the workout's id on table is " + workoutId);
+				if (user.getWorkouts().size() != 0) {
+					for (Workout workout : user.getWorkouts()) {
+						System.out.println("the workout's id in class is " + workout.getWorkoutId());
+						if (workout.getWorkoutId() == workoutId) {
+							System.out.println("workout already exists");
+							alreadyExists = true;
+						}
+					}
+				}
+				
+				if ((!alreadyExists) || user.getWorkouts().size() == 0) {
+					Workout workout = new Workout(date, time, heartRate, calories);
+					workout.setWorkoutId(workoutId);
+					user.getWorkouts().add(workout);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
 }
