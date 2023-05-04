be21fed35016588edb726664c34fe104afd0c7a3
scode234 <scodes3451@gmail.com>
2023-05-03 20:47:36 -0500

    Add deleteRow method

--- WorkoutTracker/src/com/workoutTracker/SetTable.java
+++ WorkoutTracker/src/com/workoutTracker/SetTable.java
@@ -2,14 +2,34 @@ package com.workoutTracker;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class SetTable extends Table {
 	
 	public void createTable(Connection connection) {
 		Statement statement;
 		try {
-			String query = "create table if not exists sets (setId SERIAL PRIMARY KEY, exercise SERIAL, weightLbs NUMERIC(6,1), weightKg NUMERIC(6,1), "
-					+ "reps INTEGER, FOREIGN KEY (exercise) REFERENCES exercises(exerciseId))";
+			String query = "create table if not exists sets (setId SERIAL PRIMARY KEY, exerciseId SERIAL, weightLbs NUMERIC(6,1), weightKg NUMERIC(6,1), "
+					+ "reps INTEGER, FOREIGN KEY (exerciseId) REFERENCES exercises(exerciseId))";
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void insertRow(Connection connection, ArrayList<Object> values) {
+		
+	}
+
+	public void updateRow(Connection connection, int id, ArrayList<Object> newValues) {
+		
+	}
+
+	public void deleteRow(Connection connection, int id) {
+		Statement statement;
+		try {
+			String query = String.format("delete from sets where exerciseId = '%s'", id);
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
