ef61ec729ca3121a3c7e464ba06dfc36f4bc0d84
scode234 <scodes3451@gmail.com>
2023-05-04 21:52:56 -0500

    Add readTable method

--- WorkoutTracker/src/com/workoutTracker/ExerciseTable.java
+++ WorkoutTracker/src/com/workoutTracker/ExerciseTable.java
@@ -1,7 +1,9 @@
 package com.workoutTracker;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ExerciseTable extends Table {
@@ -9,8 +11,8 @@ public class ExerciseTable extends Table {
 	public void createTable(Connection connection) {
 		Statement statement;
 		try {
-			String query = "create table if not exists exercises (exerciseId SERIAL PRIMARY KEY, workout SERIAL, type VARCHAR(20), time INTEGER,"
-					+ " FOREIGN KEY (workout) REFERENCES workouts(workoutId))";
+			String query = "create table if not exists exercises (exerciseId SERIAL PRIMARY KEY, workoutId SERIAL, type VARCHAR(20), time INTEGER,"
+					+ " FOREIGN KEY (workoutId) REFERENCES workouts(workoutId))";
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
@@ -41,6 +43,32 @@ public class ExerciseTable extends Table {
 			String query = String.format("delete from exercises where exerciseId = '%s'", id);
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
+			System.out.println("row deleted from exercise table");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void readTable(Connection connection, Workout workout, StrengthTrainingTable stTable, SetTable setTable) {
+		Statement statement;
+		ResultSet result = null;
+		try {
+			String query = "select * from exercises where workoutId = " + workout.getWorkoutId();
+			statement = connection.createStatement();
+			result = statement.executeQuery(query);
+			while (result.next()) {
+				boolean alreadyExists = false;
+				int exerciseId = Integer.valueOf(result.getString("exerciseId"));
+				int time = Integer.valueOf(result.getString("time"));
+				for (Exercise exercise : workout.getExercises()) {
+					if (exercise.getExerciseId() == exerciseId) {
+						alreadyExists = true;
+					}
+				}
+				if ((!alreadyExists) || workout.getExercises().size() == 0) {
+					stTable.readTable(connection, workout, exerciseId, time, setTable);
+				}
+			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
