57ea4fde877a2759239e05b708599aecca46c204
scode234 <scodes3451@gmail.com>
2023-05-11 20:29:13 -0500

    Initial commit

---
+++ WorkoutTracker/src/com/workoutTracker/ExerciseMenu.java
@@ -0,0 +1,21 @@
+package com.workoutTracker;
+
+import java.sql.Connection;
+
+public class ExerciseMenu extends Menu {
+
+	public void displayMenu(UserManager userManager, MainMenu mainMenu, Connection connection, UserTable userTable,
+			WorkoutTable workoutTable, ExerciseTable exerciseTable, StrengthTrainingTable stTable, SetTable setTable) {
+		
+	}
+
+	public int makeSelection() {
+	}
+
+	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection, Connection connection,
+			UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable, StrengthTrainingTable stTable,
+			SetTable setTable) {
+		
+	}
+
+}
