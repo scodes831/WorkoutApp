f5857988f9aa70e8789a77b481a72b683fc6b882
scode234 <scodes3451@gmail.com>
2023-05-22 20:58:44 -0500

    Add generateSetId method and add setId as value passed to insertRow on SetTable

--- WorkoutTracker/src/com/workoutTracker/Set.java
+++ WorkoutTracker/src/com/workoutTracker/Set.java
@@ -1,6 +1,11 @@
 package com.workoutTracker;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public class Set {
+	
+	private static int ID = 1;
 	
 	int setId;
 	double weightLbs;
@@ -8,6 +13,7 @@ public class Set {
 	int reps;
 	
 	Set(double weightLbs, int reps) {
+		this.setId = generateSetId();
 		this.weightLbs = weightLbs;
 		this.reps = reps;
 	}
@@ -17,6 +23,23 @@ public class Set {
 		this.weightLbs = weightLbs;
 		this.weightKg = weightKg;
 		this.reps = reps;
+	}
+	
+	private int generateSetId() {
+		int id = Set.getID();
+		id++;
+		Set.setID(id);
+		return id;
+	}
+	
+	public void addToSetTable(Connection connection, StrengthTraining st, SetTable setTable) {
+		ArrayList<Object> values = new ArrayList<Object>();
+		values.add(getSetId());
+		values.add(st.getExerciseId());
+		values.add(getWeightLbs());
+		values.add(getWeightKg());
+		values.add(getReps());
+		setTable.insertRow(connection, values);
 	}
 	
 	public double getWeightLbs() {
@@ -44,5 +67,13 @@ public class Set {
 
 	public void setSetId(int setId) {
 		this.setId = setId;
+	}
+
+	public static int getID() {
+		return ID;
+	}
+
+	public static void setID(int iD) {
+		ID = iD;
 	}
 }
