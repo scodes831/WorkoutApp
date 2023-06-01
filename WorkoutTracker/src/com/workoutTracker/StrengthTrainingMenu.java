9fb298d1e0f011d0967271e81c016cda0d70a34e
scode234 <scodes3451@gmail.com>
2023-05-31 19:31:50 -0500

    Add parameters for bikeTable, runTable, hiitTable

--- WorkoutTracker/src/com/workoutTracker/StrengthTrainingMenu.java
+++ WorkoutTracker/src/com/workoutTracker/StrengthTrainingMenu.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class StrengthTrainingMenu {
 
 	public void displayMenu(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu, ExerciseMenu exMenu, Connection connection,
-			UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable, StrengthTrainingTable stTable,
+			UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable,
 			SetTable setTable, Exercise exercise) {
 		boolean inputError = false;
 		do {
 			try {
 				int selection = makeSelection();
-				processSelection(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, stTable, setTable, exMenu, exercise, selection);
+				processSelection(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exMenu, exercise, selection);
 			} catch (Exception e) {
 				System.out.println(e);
 			}
@@ -21,7 +21,7 @@ public class StrengthTrainingMenu {
 	}
 
 	public void processSelection(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu, Connection connection, UserTable userTable,
-			WorkoutTable workoutTable, ExerciseTable exerciseTable, StrengthTrainingTable stTable, SetTable setTable, ExerciseMenu exMenu, Exercise exercise, int selection) {
+			WorkoutTable workoutTable, ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable, SetTable setTable, ExerciseMenu exMenu, Exercise exercise, int selection) {
 		StrengthTraining st = (StrengthTraining) exercise;
 		switch (selection) {
 		case 1:
@@ -34,10 +34,10 @@ public class StrengthTrainingMenu {
 			st.editStrengthTrainingDetails(connection, workout, exerciseTable, stTable);
 			break;
 		case 3:
-			exMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, stTable, setTable);
+			exMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 			break;
 		}
-		displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, connection, userTable, workoutTable, exerciseTable, stTable, setTable, exercise);
+		displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exercise);
 	}
 
 	public int makeSelection() {
