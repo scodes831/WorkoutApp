6be124d62ad2c10541cb1fb2c3bb8eeb5e987c6b
scode234 <scodes3451@gmail.com>
2023-06-04 20:07:58 -0500

    Refactor readTable method

--- WorkoutTracker/src/com/workoutTracker/StrengthTrainingTable.java
+++ WorkoutTracker/src/com/workoutTracker/StrengthTrainingTable.java
@@ -10,7 +10,7 @@ public class StrengthTrainingTable extends Table {
 	public void createTable(Connection connection) {
 		Statement statement;
 		try {
-			String query = "create table if not exists strengthTraining (strengthtrainingId SERIAL PRIMARY KEY, exerciseId SERIAL, "
+			String query = "create table if not exists strengthTraining (exerciseId INTEGER PRIMARY KEY, "
 					+ "exerciseName VARCHAR(40), muscleGroup VARCHAR(20), FOREIGN KEY (exerciseId) REFERENCES exercises(exerciseId))";
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
@@ -23,8 +23,8 @@ public class StrengthTrainingTable extends Table {
 		Statement statement;
 		try {
 			String query = String.format(
-					"insert into strengthtraining (exerciseName, muscleGroup) values ('%s', '%s');",
-					values.get(0), values.get(1));
+					"insert into strengthtraining (exerciseId, exerciseName, muscleGroup) values ('%s', '%s', '%s');", values.get(0),
+					values.get(1), values.get(2));
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
@@ -33,7 +33,18 @@ public class StrengthTrainingTable extends Table {
 	}
 
 	public void updateRow(Connection connection, int id, ArrayList<Object> newValues) {
-		
+		Statement statement;
+		try {
+			String query = String.format(
+					"update strengthtraining set exercisename = '%s', musclegroup = '%s' where exerciseid = '%s'",
+					newValues.get(0), newValues.get(1), id);
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
 
 	public void deleteRow(Connection connection, int id) {
@@ -42,25 +53,25 @@ public class StrengthTrainingTable extends Table {
 			String query = String.format("delete from strengthtraining where exerciseId = '%s'", id);
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
-			
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	public void readTable(Connection connection, Workout workout, int exerciseId, int exerciseTime, SetTable setTable) {
+
+	public void readTable(Connection connection, Workout workout, Exercise exercise, SetTable setTable) {
 		Statement statement;
 		ResultSet result = null;
 		try {
-			String query = "select * from strengthtraining where exerciseId = " + exerciseId;
+			String query = "select * from strengthtraining where exerciseId = " + exercise.getExerciseId();
 			statement = connection.createStatement();
 			result = statement.executeQuery(query);
 			while (result.next()) {
 				String exerciseName = result.getString("exercisename");
 				String muscleGroup = result.getString("musclegroup");
-				StrengthTraining st = new StrengthTraining(exerciseId, exerciseTime, exerciseName, muscleGroup);
-				workout.getExercises().add(st);
-				setTable.readTable(connection, st);
+				((StrengthTraining) exercise).setExerciseName(exerciseName);
+				((StrengthTraining) exercise).setMuscleGroup(muscleGroup);
+				setTable.readTable(connection, (StrengthTraining) exercise);
 			}
 		} catch (Exception e) {
 			System.out.println(e);
