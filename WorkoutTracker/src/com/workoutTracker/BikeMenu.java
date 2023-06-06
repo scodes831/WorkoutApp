35f15e3f5bb81bccd9b656d869a383ed25846d69
scode234 <scodes3451@gmail.com>
2023-06-05 20:21:24 -0500

    Update parameters

--- WorkoutTracker/src/com/workoutTracker/BikeMenu.java
+++ WorkoutTracker/src/com/workoutTracker/BikeMenu.java
@@ -1,5 +1,55 @@
 package com.workoutTracker;
 
-public class BikeMenu {
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class BikeMenu extends SubMenu {
+
+	public void displayMenu(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu,
+			ExerciseMenu exMenu, Connection connection, UserTable userTable, WorkoutTable workoutTable,
+			ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable,
+			StrengthTrainingTable stTable, SetTable setTable, Exercise exercise) {
+		boolean inputError = false;
+		do {
+			try {
+				int selection = makeSelection();
+				processSelection(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable,
+						exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exMenu, exercise, selection);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		} while (inputError);
+	}
+
+	public void processSelection(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu,
+			Connection connection, UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable,
+			BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable,
+			SetTable setTable, ExerciseMenu exMenu, Exercise exercise, int selection) {
+		Bike bike = (Bike) exercise;
+		switch (selection) {
+		case 1:
+			bike.displayBikeExercises();
+			break;
+		case 2:
+			bike.displayBikeExercises();
+			bike.editBikeDetails(connection, workout, exerciseTable, bikeTable);
+			break;
+		case 3:
+			exMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable,
+					exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
+			break;
+		}
+		displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, connection, userTable, workoutTable,
+				exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exercise);
+	}
+
+	public int makeSelection() {
+		System.out.println(
+				"Bike Menu Options:\n1 - View Bike Exercise Details\n2 - Edit Exercise\n3 - Back to Exercise Menu");
+		Scanner in = new Scanner(System.in);
+		int selection = in.nextInt();
+		return selection;
+	}
 
 }
