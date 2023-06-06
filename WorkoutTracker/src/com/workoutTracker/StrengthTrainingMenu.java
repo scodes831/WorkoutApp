135c4f10ef5a87d031106d1388e7251c63450557
scode234 <scodes3451@gmail.com>
2023-06-05 20:21:49 -0500

    Update parameters

--- WorkoutTracker/src/com/workoutTracker/StrengthTrainingMenu.java
+++ WorkoutTracker/src/com/workoutTracker/StrengthTrainingMenu.java
@@ -3,16 +3,18 @@ package com.workoutTracker;
 import java.sql.Connection;
 import java.util.Scanner;
 
-public class StrengthTrainingMenu {
+public class StrengthTrainingMenu extends SubMenu {
 
-	public void displayMenu(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu, ExerciseMenu exMenu, Connection connection,
-			UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable,
-			SetTable setTable, Exercise exercise) {
+	public void displayMenu(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu,
+			ExerciseMenu exMenu, Connection connection, UserTable userTable, WorkoutTable workoutTable,
+			ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable,
+			StrengthTrainingTable stTable, SetTable setTable, Exercise exercise) {
 		boolean inputError = false;
 		do {
 			try {
 				int selection = makeSelection();
-				processSelection(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exMenu, exercise, selection);
+				processSelection(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable,
+						exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exMenu, exercise, selection);
 			} catch (Exception e) {
 				System.out.println(e);
 			}
@@ -20,24 +22,28 @@ public class StrengthTrainingMenu {
 		} while (inputError);
 	}
 
-	public void processSelection(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu, Connection connection, UserTable userTable,
-			WorkoutTable workoutTable, ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable, SetTable setTable, ExerciseMenu exMenu, Exercise exercise, int selection) {
+	public void processSelection(Workout workout, UserManager userManager, MainMenu mainMenu, WorkoutMenu workoutMenu,
+			Connection connection, UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable,
+			BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable,
+			SetTable setTable, ExerciseMenu exMenu, Exercise exercise, int selection) {
 		StrengthTraining st = (StrengthTraining) exercise;
 		switch (selection) {
 		case 1:
 			st.displayStrengthTrainingExericses();
 			SetMenu setMenu = new SetMenu();
-			setMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, this, connection, userTable, workoutTable, exerciseTable, stTable, setTable, st);
+			setMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, this, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, st);
 			break;
 		case 2:
 			st.displayStrengthTrainingExericses();
 			st.editStrengthTrainingDetails(connection, workout, exerciseTable, stTable);
 			break;
 		case 3:
-			exMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
+			exMenu.displayMenu(workout, userManager, mainMenu, workoutMenu, connection, userTable, workoutTable,
+					exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 			break;
 		}
-		displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exercise);
+		displayMenu(workout, userManager, mainMenu, workoutMenu, exMenu, connection, userTable, workoutTable,
+				exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable, exercise);
 	}
 
 	public int makeSelection() {
