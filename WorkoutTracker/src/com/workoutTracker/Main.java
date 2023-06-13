009b895e7d5b745d91d6ea74e4c88037aa170846
scode234 <scodes3451@gmail.com>
2023-06-12 19:47:25 -0500

    Create and read tables

--- WorkoutTracker/src/com/workoutTracker/Main.java
+++ WorkoutTracker/src/com/workoutTracker/Main.java
@@ -1,13 +1,31 @@
 package com.workoutTracker;
 
+import java.sql.Connection;
+
 public class Main {
 
 	public static void main(String[] args) {
 		DatabaseManager dm = new DatabaseManager();
-		dm.connectDatabase("workouttracker", null, null);
+		Connection connection = dm.connectDatabase("workouttracker", null, null);
+		UserTable userTable = new UserTable();
+		WorkoutTable workoutTable = new WorkoutTable();
+		ExerciseTable exerciseTable = new ExerciseTable();
+		BikeTable bikeTable = new BikeTable();
+		RunTable runTable = new RunTable();
+		HIITTable hiitTable = new HIITTable();
+		StrengthTrainingTable stTable = new StrengthTrainingTable();
+		SetTable setTable = new SetTable();
+		userTable.createTable(connection);
+		workoutTable.createTable(connection);
+		exerciseTable.createTable(connection);
+		bikeTable.createTable(connection);
+		runTable.createTable(connection);
+		hiitTable.createTable(connection);
+		stTable.createTable(connection);
+		setTable.createTable(connection);
 		UserManager um = new UserManager();
 		MainMenu mainMenu = new MainMenu();
-		UserMenu userMenu = new UserMenu();
-		mainMenu.displayMenu(um, mainMenu);
+		userTable.readTable(connection, um, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
+		mainMenu.displayMenu(um, mainMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 	}
 }
