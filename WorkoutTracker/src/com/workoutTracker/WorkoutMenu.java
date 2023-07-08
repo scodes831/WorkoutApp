aa8c5a80e6f1d39d5f6480c785bcfbd93f5a44d0
scode234 <scodes3451@gmail.com>
2023-07-08 18:59:26 -0500

    Refactor processSelection to resolve issue of asking for userId when going back to main menu

--- WorkoutTracker/src/com/workoutTracker/WorkoutMenu.java
+++ WorkoutTracker/src/com/workoutTracker/WorkoutMenu.java
@@ -22,39 +22,39 @@ public class WorkoutMenu extends Menu {
 
 	public void processSelection(UserManager userManager, MainMenu mainMenu, int selection, Connection connection, UserTable userTable,
 			WorkoutTable workoutTable, ExerciseTable exerciseTable, BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable, SetTable setTable) {
-		User selectedUser = userManager.selectAUser(userManager);
 		switch (selection) {
 		case 1:
+			User selectedUser = userManager.selectAUser(userManager);
 			Workout workout = selectedUser.addWorkout(selectedUser);
 			selectedUser.addWorkoutToDatabase(workout, connection, workoutTable);
 			workout.addWorkoutDetails(workout, connection, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 			break;
 		case 2:
-			if (selectedUser.getWorkouts().size() == 0) {
-				System.out.println("User " + selectedUser.getFirstName() + " " + selectedUser.getLastName() + " has 0 workouts to display.\n");
+			User selectedUser2 = userManager.selectAUser(userManager);
+			if (selectedUser2.getWorkouts().size() == 0) {
+				System.out.println("User " + selectedUser2.getFirstName() + " " + selectedUser2.getLastName() + " has 0 workouts to display.\n");
 				break;
 			} else {
-				selectedUser.displayAllWorkouts();
-				Workout selectedWorkout = selectedUser.selectWorkout();
+				selectedUser2.displayAllWorkouts();
+				Workout selectedWorkout = selectedUser2.selectWorkout();
 				int selectedWorkoutOption = selectWorkoutOptions();
 				if (selectedWorkoutOption == 1) {
 					selectedWorkout.displayExercises();
 					ExerciseMenu exerciseMenu = new ExerciseMenu();
 					exerciseMenu.displayMenu(selectedWorkout, userManager, mainMenu, this, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 				} else if (selectedWorkoutOption == 2) {
-					selectedWorkout.editWorkout(connection, selectedUser, workoutTable, exerciseTable, bikeTable, hiitTable, runTable, stTable, setTable);
+					selectedWorkout.editWorkout(connection, selectedUser2, workoutTable, exerciseTable, bikeTable, hiitTable, runTable, stTable, setTable);
 				} else {
 					break;
 				}
 				break;
 			}
-			
 		case 3:
 			mainMenu.displayMenu(userManager, mainMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 		}
 		displayMenu(userManager, mainMenu, connection, userTable, workoutTable, exerciseTable, bikeTable, runTable, hiitTable, stTable, setTable);
 	}
-
+	
 	public int makeSelection() {
 		System.out.println(
 				"Workout Menu Options:\n1 - Add Workout\n2 - Display Workouts\n3 - Back to Main Menu");
