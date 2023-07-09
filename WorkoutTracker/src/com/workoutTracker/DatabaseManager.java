d603a31b8903483eb4c64e9f1414123f1cbba5bd
scode234 <scodes3451@gmail.com>
2023-07-08 19:09:29 -0500

    Add createTables method

--- WorkoutTracker/src/com/workoutTracker/DatabaseManager.java
+++ WorkoutTracker/src/com/workoutTracker/DatabaseManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseManager {
-	
+
 	public Connection connectDatabase(String database, String user, String pw) {
 		Connection connection = null;
 		try {
@@ -14,5 +14,18 @@ public class DatabaseManager {
 			System.out.println(e);
 		}
 		return connection;
+	}
+
+	public void createTables(Connection connection, UserTable userTable, WorkoutTable workoutTable, ExerciseTable exerciseTable,
+			BikeTable bikeTable, RunTable runTable, HIITTable hiitTable, StrengthTrainingTable stTable,
+			SetTable setTable) {
+		userTable.createTable(connection);
+		workoutTable.createTable(connection);
+		exerciseTable.createTable(connection);
+		bikeTable.createTable(connection);
+		runTable.createTable(connection);
+		hiitTable.createTable(connection);
+		stTable.createTable(connection);
+		setTable.createTable(connection);
 	}
 }
